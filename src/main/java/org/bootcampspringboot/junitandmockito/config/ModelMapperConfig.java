838b268d205e795b523c6049a396871f4a83d15b
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-03 10:43:47 -0300

    Model Mapper Config

---
+++ src/main/java/org/bootcampspringboot/junitandmockito/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package org.bootcampspringboot.junitandmockito.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
+}
