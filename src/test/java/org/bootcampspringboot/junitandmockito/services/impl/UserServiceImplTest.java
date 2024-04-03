2544972ee28f9d6c9b388a6485f113e8b5e7910d
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-03 20:09:05 -0300

    Teste para classe UserServiceImplTest metodo delete com sucesso

--- src/test/java/org/bootcampspringboot/junitandmockito/services/impl/UserServiceImplTest.java
+++ src/test/java/org/bootcampspringboot/junitandmockito/services/impl/UserServiceImplTest.java
@@ -22,7 +22,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -146,18 +146,15 @@ class UserServiceImplTest {
             assertEquals("E-mail already on database", e.getMessage());
         }
     }
-    @Test
-    void update() {
-    }
+
 
     @Test
-    void delete() {
+    void whenDeleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
-    private void startUser(){
-
-
-
-    }
 
 }
