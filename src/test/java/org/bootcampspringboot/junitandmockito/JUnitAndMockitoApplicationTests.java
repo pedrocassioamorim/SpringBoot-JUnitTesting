26cb59189c2e257cbdfd05046df54d4404184cba
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-03 16:54:36 -0300

    Teste para classe principal

--- src/test/java/org/bootcampspringboot/junitandmockito/JUnitAndMockitoApplicationTests.java
+++ src/test/java/org/bootcampspringboot/junitandmockito/JUnitAndMockitoApplicationTests.java
@@ -1,5 +1,6 @@
 package org.bootcampspringboot.junitandmockito;
 
+import org.apiguardian.api.API;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JUnitAndMockitoApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        JUnitAndMockitoApplication.main(new String[] {});
     }
 
 }
