f5420b8c3c9f391f33150197521f662e26a2fa08
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-04-03 21:00:40 -0300

    Teste para endpoint Create da classe UserResource implementado

--- src/test/java/org/bootcampspringboot/junitandmockito/resources/UserResourceTest.java
+++ src/test/java/org/bootcampspringboot/junitandmockito/resources/UserResourceTest.java
@@ -97,7 +97,15 @@ class UserResourceTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnCreated() {
+        when(service.create(any())).thenReturn(user);
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+        assertEquals(ResponseEntity.class, response.getClass());
+
+
     }
 
     @Test
